b9a4c545346f982d41a91db99e92374f56e60ccb
Brayhan De Aza <brayhandeaza@gmail.com>
2025-12-11 23:13:41 -0500

    refactor ast structure

--- src/core/core.h
+++ src/core/core.h
@@ -5,7 +5,7 @@
 #ifndef CROMIO_LOWERING_H
 #define CROMIO_LOWERING_H
 
-#include "emitter/CodeEmitter.h"
-#include "ir/IR.h"
+#include "core/llvm/emitter/CodeEmitter.h"
+#include "core/llvm/ir/IR.h"
 
-#endif //CROMIO_LOWERING_H
+#endif // CROMIO_LOWERING_H
