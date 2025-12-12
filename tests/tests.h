579b5bc045311e6d878f686b11f2b425aff0f824
Brayhan De Aza <brayhandeaza@gmail.com>
2025-12-12 07:19:17 -0500

    add test libs

---
+++ tests/tests.h
@@ -0,0 +1,7 @@
+//
+// Created by Brayhan De Aza on 12/12/25.
+//
+
+#pragma once
+
+#include "variables/variables.h"
---
+++ tests/variables/variables.h
@@ -0,0 +1,11 @@
+//
+// Created by Brayhan De Aza on 12/12/25.
+//
+
+#pragma once
+
+#include "libs/catch2.hpp"
+
+TEST_CASE("Variable Declarations") {
+
+}
